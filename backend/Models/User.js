439a50f39619ba9f90ed793a7a4cafc8c23e5a31
Raj Roy <rajroy894290@gmail.com>
2024-02-06 12:43:13 +0530

    setup react in frontend and expressJs in backend and also create schema for users, song & playlist using mongoose

---
+++ backend/Models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+//step 1: import mongoose
+//step 2: create a schema (structure of a User)
+//step 3: create a model
+
+const User = new mongoose.Schema({
+    firstName: {
+        type:String,
+        required: true
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    likedSong: {
+        type: String,
+        //we change it to array later
+        default:''
+    },
+    likedPlaylist: {
+        //we change it to array later
+        type: String,
+        default: ''
+    },
+    subscribedArtist: {
+        //we change it to array later
+        type: String,
+        default: ''
+    }
+});
+const UserModel = mongoose.model('User',User);
+module.exports = UserModel;
