ee81a78c4e1ca474fe687d04b1630d9a3567a823
Raj Roy <rajroy894290@gmail.com>
2024-02-06 13:29:13 +0530

    connect mongoDB database to our express app

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+require('dotenv').config();
 const port = 8000;
+const mongoDb = `mongodb+srv://rajroy:${process.env.MONGO_PASSWORD}@cluster0.etidfhl.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect(mongoDb).then((x)=>{
+    console.log('mongoDB atlas successfully connected to express Js')
+}).catch((err)=>{
+    console.error(err);
+})
 
 app.get('/',(req,res)=>{
     res.send('Hello from express');
