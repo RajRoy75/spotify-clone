77a1c233e78cdb06136379727367fb12a477f910
Raj Roy <rajroy894290@gmail.com>
2024-02-09 23:20:40 +0530

    add intial code of passport.js for authentication

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const User = require('./Models/User');
 
 const app = express();
 require('dotenv').config();
@@ -9,7 +13,25 @@ mongoose.connect(mongoDb).then((x)=>{
     console.log('mongoDB atlas successfully connected to express Js')
 }).catch((err)=>{
     console.error(err);
-})
+});
+// adding passport-jwt
+
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'ThisIsAsecretKey';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
 
 app.get('/',(req,res)=>{
     res.send('Hello from express');
