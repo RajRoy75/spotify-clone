91d02744ac588354ece3ae6824eb342e31327cb2
Raj Roy <rajroy894290@gmail.com>
2024-03-18 22:07:12 +0530

    complete the full authentication system, connect frontend to backend, showing different routes for logged in person

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,45 +5,53 @@ const { getToken } = require('../utils/helpers');
 const route = express.Router();
 
 
-route.post('/register', async(req,res)=>{
+
+route.post('/register', async (req, res) => {
     // step 1 : get all this details from frontend site
-    const {email,userName,firstName,lastName,password} = req.body;
+    const { email, userName, firstName, lastName, password } = req.body;
 
     //step 2 : check user is already exist or not
     //step 2.1 : get email id from DB
-    const user = await User.findOne({email: email});
-    if(user){
-        res.status(404).json({error : "This user is already exist"});
+    const user = await User.findOne({ email: email });
+    if (user) {
+        res.status(404).json({ error: "This user is already exist" });
     }
     //step 3 : add user to our DB
     //step 3.1 : Hashing the password
-    const hashPassword = await bcrypt.hash(password,10);
-    const newUserData = {email,userName,firstName,lastName,password:hashPassword};
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUserData = { email, userName, firstName, lastName, password: hashPassword };
     const createNewUser = await User.create(newUserData);
     //step 4: create JWT token
-    const token = await getToken(email,createNewUser);
+    const token = await getToken(email, createNewUser);
     //step 5: return all the data to the user
     // const returnToUser = createNewUser.toObject();
-    const returnToUser = {...createNewUser.toObject(),token};
+    const returnToUser = { ...createNewUser.toObject(), token };
     delete returnToUser.password;
     res.json(returnToUser);
 });
-route.post('/login', async(req,res)=>{
-    const {email,password} = req.body;
-    const user = await User.findOne({email: email});
-    if(!user){
-        res.status(404).json({error: 'Invalid login credential'});
+route.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            res.status(404).json({ error: 'Invalid login credential' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(404).json({ error: 'invalid login credential' });
+        }
+        const token = await getToken(email, user);
+        //step 5: return all the data to the user
+        // const returnToUser = createNewUser.toObject();
+        const returnToUser = { ...user.toObject(), token };
+        delete returnToUser.password;
+        res.json(returnToUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Enternal server error" })
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if(!isPasswordValid){
-        res.status(404).json({error: 'invalid login credential'});
-    }
-    const token = await getToken(email,user);
-    //step 5: return all the data to the user
-    // const returnToUser = createNewUser.toObject();
-    const returnToUser = {...user.toObject(),token};
-    delete returnToUser.password;
-    res.json(returnToUser);
+
 
 })
+
 module.exports = route;
